6ad2a8d075ee1749dd79acc55ccc366a5559b63a
John Cantrell <johncantrell97@protonmail.com>
2020-02-05 15:09:10 -0500

    use patched protobufjs from juggernaut/proto-loader

--- src/service.js
+++ src/service.js
@@ -1,8 +1,8 @@
 import { join } from 'path'
 import EventEmitter from 'events'
 import defaultsDeep from 'lodash.defaultsdeep'
-import { credentials, loadPackageDefinition, status, Metadata } from '@grpc/grpc-js'
-import { load } from '@ln-zap/proto-loader'
+import { credentials, loadPackageDefinition, status } from '@grpc/grpc-js'
+import { load } from '@ln-juggernaut/proto-loader'
 import StateMachine from 'javascript-state-machine'
 import debug from 'debug'
 import {
